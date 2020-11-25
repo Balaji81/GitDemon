b5957d6a736991cd5e236304025cec793a23e9de
balaji81 <balajisaik@yahoo.com>
2020-11-25 08:06:23 +0530

    another try

--- src/com/first/Hello.java
+++ src/com/first/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hello world.. ");
-      System.out.println("Hello new GIT..(Branch)");
+      System.out.println("Hello new GIT..(Branch).");
       System.out.println("Hello Balaji ...");
       System.out.println("Hello Feature 2 branch ...");
 	}
